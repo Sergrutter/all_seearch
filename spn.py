a020da8a5c956cf37dd87792431646276a737488
Sergrutter <134686706+Sergrutter@users.noreply.github.com>
2025-05-03 12:27:11 +0300

    Create spn.py

---
+++ spn.py
@@ -0,0 +1,8 @@
+def get_spn(toponym):
+    envelope = toponym["boundedBy"]["Envelope"]
+    lower_corner = list(map(float, envelope["lowerCorner"].split()))
+    upper_corner = list(map(float, envelope["upperCorner"].split()))
+
+    dx = abs(upper_corner[0] - lower_corner[0])
+    dy = abs(upper_corner[1] - lower_corner[1])
+    return str(dx), str(dy)
